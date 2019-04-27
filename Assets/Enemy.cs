430c5e87893e7416c91d2d45a3697a2c9e256a16
jckong99 <jckong99@gmail.com>
2019-04-27 19:19:45 -0400

    First iteration of IEntity, Enemy, and EnemyHorde classes

---
+++ Assets/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0f;
+
+    private static int idSource = 0;
+    private Vector3 targetPosition, previousPosition;
+
+    /// <summary>
+    /// Unique Enemy ID number; IDs are assigned incrementally from 0.
+    /// </summary>
+    public int Id { get; private set; }
+
+    private void Start()
+    {
+        Id = idSource;
+        idSource++;
+    }
+
+    private void Update()
+    {
+        UpdateGridPosition();
+    }
+
+    // Moves Enemy in straight line towards intermedate targetPosition; updates relevant EnemyHorde references when necessary
+    private void UpdateGridPosition()
+    {
+        GameManager gameManager = GameManager.GetInstance();
+        IEntity entity;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        if ((transform.position - previousPosition).sqrMagnitude >= Mathf.Pow(0.5 * gameManager.TILE_SIZE, 2))
+        {
+            if ((entity = gameManager.EntityAt(targetPosition.x, targetPosition.y)) is EnemyHorde)
+                ((EnemyHorde)entity).AddEnemy(this);
+            if ((entity = gameManager.EntityAt(previousPosition.x, previousPosition.y)) is EnemyHorde)
+                ((EnemyHorde)entity).RemoveEnemy(this);
+        }
+    }
+}
---
+++ Assets/EnemyHorde.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class EnemyHorde
+{
+    private Dictionary<int, Enemy> horde = new Dictionary<int, Enemy>();
+
+    /// <summary>
+    /// Adds specified Enemy to internal Dictionary, mapping ID to Enemy object.
+    /// </summary>
+    /// <param name="enemy"></param>
+    public void AddEnemy(Enemy enemy)
+    {
+        horde.Add(enemy.Id, enemy);
+    }
+
+    /// <summary>
+    /// Removes Enemy from internal Dictionary.
+    /// </summary>
+    /// <param name="enemy"></param>
+    public void RemoveEnemy(Enemy enemy)
+    {
+        horde.Remove(enemy.Id);
+    }
+}
