a737fa7b3696e8a998066af7dcd8e3377643131e
Vincent <chozorho@Windows>
2019-04-27 22:04:02 -0400

    Include plantHistory hashmap to compute harvest returns.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,26 +8,27 @@ public enum Weather { Sun, Clouds, Rain };
 public class GameManager : MonoBehaviour
 {
     private static GameManager gameManager;
+    [SerializeField] private GameObject plantPrefab = null;
 
-    private int Width, Height;
-    private IEntity[,] GameBoard;
+    private int width, height;
+    private IEntity[,] gameBoard;
+    private Dictionary<Plant, uint> plantHistory;
 
     /* GLOBAL STATS */
     /* The only one we should need to share with the user is the SeedCount. */
-    private uint PlantCount;
+    private uint plantCount;
     private uint ERPCount;
-    private uint SeedCount;
-    private int Hydration;
-    private float Nutrients;
-    private float RemainingTime;
+    private uint seedCount;
+    private int hydration;
+    private float nutrients;
+    private float remainingTime;
     private const float RESET_TIME = 300;
-    private uint CurrentCycle;
-    private uint GoalCycle;
+    private uint currentCycle;
+    private uint goalCycle;
     public const float TILE_SIZE = 0.500f;
 
-
-    private Phase CurrentPhase;
-    private Weather CurrentWeather;
+    private Phase currentPhase;
+    private Weather currentWeather;
 
     public static GameManager GetInstance()
     {
@@ -35,7 +36,7 @@ public class GameManager : MonoBehaviour
     }
 
     public IEntity EntityAt(int x, int y) {
-        return GameBoard[y, x];
+        return gameBoard[y, x];
     }
 
     private void Awake()
@@ -58,31 +59,41 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        Width = 10;
-        Height = 10;
-        GameBoard = new IEntity[Height, Width];
+        /* Init data structures */
+        width = 10;
+        height = 10;
+        gameBoard = new IEntity[height, width];
+        for (int r=0; r < height; r ++)
+        {
+            for (int c = 0; c < width; c++)
+            {
+ /*               gameBoard[r,c] = Instantiate(plantPrefab, new Vector3(c * TILE_SIZE, r * TILE_SIZE, 0), Quaternion.identity);*/
+            }
+        }
+
+        plantHistory = new Dictionary<Plant, uint>();
 
         /* Initialize at dawn. */
-        CurrentPhase = Phase.Dawn;
-        CurrentWeather = Weather.Sun;
+        currentPhase = Phase.Dawn;
+        currentWeather = Weather.Sun;
 
-        PlantCount = 0;
+        plantCount = 0;
         ERPCount = 0;
-        SeedCount = 1;
-        Hydration = 10; // on a scale 0 - 20?
-        Nutrients = 4.50f; // on a scale 0 - 10?
-        RemainingTime = RESET_TIME;
+        seedCount = 1;
+        hydration = 10; // on a scale 0 - 20?
+        nutrients = 4.50f; // on a scale 0 - 10?
+        remainingTime = RESET_TIME;
     }
 
     /* To be called from the Button object */
     public void AdvancePhase()
     {
-        if (CurrentPhase == Phase.Dawn)
+        if (currentPhase == Phase.Dawn)
         {
-            CurrentPhase = Phase.Day;
-        } else if (CurrentPhase == Phase.Day)
+            currentPhase = Phase.Day;
+        } else if (currentPhase == Phase.Day)
         {
-            CurrentPhase = Phase.Night;
+            currentPhase = Phase.Night;
         } /*else if (RemainingTime <= 0)
         {
             CurrentPhase = Phase.Dawn;
@@ -90,30 +101,43 @@ public class GameManager : MonoBehaviour
         }*/ // should not need this...
     }
 
+    /* Precondition: x and y are nonnegative integers that are valid
+     * game board indices
+     * Postcondition: processes the addition of this plant object,
+     * by adding it to the game board and also to the plant history 
+     * data structure. */
+    public void AddPlant(uint x, uint y)
+    {
+        Plant Spawn/* = Instantiate(plantPrefab, new Vector3(x*TILE_SIZE, y*TILE_SIZE, 0), Quaternion.identity)*/;
+        plantHistory.Add(Spawn, currentCycle);
+        gameBoard[y,x] = Spawn;
+    }
+
     /* To be called by a Plant, whenever it is extracted. */
     public void ExtractPlant(Plant extracted)
     {
-        GameBoard[extracted.Y, extracted.X] = null;
-        PlantCount--;
+        gameBoard[extracted.Y, extracted.X] = null;
+        plantCount--;
         ERPCount++;
 
         /* TODO process the "Return," which means
          * increasing the Hydration and/or Nutrients globally. */
-        Hydration = Mathf.Max(20, Hydration + extracted.DaysAlive);
-        Nutrients += Mathf.Max(10.0f, Nutrients + (0.1f*extracted.DaysAlive));
+        uint daysAlive = currentCycle - plantHistory[extracted];
+        hydration = Mathf.Max(20, hydration + (int)daysAlive);
+        nutrients += Mathf.Max(10.0f, nutrients + (0.1f*daysAlive));
     }
 
     /* To be called by a Plant OR Pathogen object, whenever it kills a plant. */
     public void KillPlant(Plant debilitated)
     {
-        GameBoard[debilitated.Y, debilitated.X] = null;
-        PlantCount--;
+        gameBoard[debilitated.Y, debilitated.X] = null;
+        plantCount--;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        switch (CurrentPhase)
+        switch (currentPhase)
         {
             case Phase.Dawn:
 
@@ -122,8 +146,8 @@ public class GameManager : MonoBehaviour
                 
                 break;
             case Phase.Night:
-                RemainingTime -= Time.deltaTime;
-                if (RemainingTime <= 0)
+                remainingTime -= Time.deltaTime;
+                if (remainingTime <= 0)
                 {
                     AdvancePhase();
                 }
